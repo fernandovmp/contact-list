3d79277edfd2531e9cca14c7f6a712a2cdb3c178
Fernando Mariano Pereira <fernandovmp@outlook.com>
2020-01-30 14:58:00 -0300

    Add delete button to contact card

--- web-client/src/components/ContactCard/index.tsx
+++ web-client/src/components/ContactCard/index.tsx
@@ -1,18 +1,35 @@
 import React from 'react';
 import Contact from '../../models/Contact';
-import './styles.css'
+import { IconButton, IButtonStyles } from 'office-ui-fabric-react/lib/Button';
+import './styles.css';
 
 type propsType = {
     contact: Contact;
+    deleteCommand: (contact: Contact) => void;
 };
 
-const ContactCard: React.FC<propsType> = ({ contact }) => {
+const fabricButtonStyles: IButtonStyles = {
+    icon: [
+        {
+            color: '#ff0000'
+        }
+    ]
+};
+
+const ContactCard: React.FC<propsType> = ({ contact, deleteCommand }) => {
     return (
-      <div className="contact-card" >
-        <strong className="name" >{contact.name}</strong>
-        <p>{`Email: ${contact.email ?? '?'}`}</p>
-        <p>{`Telefone: ${contact.phone ?? '?'}`}</p>
-      </div>
+        <div className="contact-card">
+            <header className="contact-header">
+                <strong className="name">{contact.name}</strong>
+                <IconButton
+                    iconProps={{ iconName: 'Delete' }}
+                    styles={fabricButtonStyles}
+                    onClick={e => deleteCommand(contact)}
+                />
+            </header>
+            <p>{`Email: ${contact.email ?? '?'}`}</p>
+            <p>{`Telefone: ${contact.phone ?? '?'}`}</p>
+        </div>
     );
 };
 
