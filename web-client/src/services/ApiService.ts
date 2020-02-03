1fd7fed2709e086bb2aebb1ea6ca61590a69ac4e
Fernando Mariano Pereira <fernandovmp@outlook.com>
2020-02-03 19:25:31 -0300

    Add get single contact and update contact api calls

--- web-client/src/services/ApiService.ts
+++ web-client/src/services/ApiService.ts
@@ -2,7 +2,9 @@ import Contact from '../models/Contact';
 
 export interface IApiService {
     getAllContacts: () => Promise<Contact[]>;
+    getContact: (id: string) => Promise<Contact>;
     createContact: (contact: Contact) => Promise<Contact>;
+    updateContact: (contact: Contact) => Promise<Contact>;
     deleteContact: (contact: Contact) => Promise<void>;
 }
 
@@ -17,6 +19,12 @@ export class ApiService implements IApiService {
         const response = await fetch(`${this.baseUrl}/api/contacts`);
         return response.json();
     }
+
+    async getContact(id: string): Promise<Contact> {
+        const response = await fetch(`${this.baseUrl}/api/contacts/${id}`);
+        return response.json();
+    }
+
     async createContact(contact: Contact): Promise<Contact> {
         const response = await fetch(`${this.baseUrl}/api/contacts`, {
             method: 'POST',
@@ -28,6 +36,19 @@ export class ApiService implements IApiService {
         });
         return response.json();
     }
+
+    async updateContact(contact: Contact): Promise<Contact> {
+        const response = await fetch(`${this.baseUrl}/api/contacts/${contact.id}`, {
+            method: 'PUT',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(contact)
+        });
+        return response.json();
+    }
+
     async deleteContact(contact: Contact): Promise<void> {
         await fetch(`${this.baseUrl}/api/contacts/${contact.id}`, {
             method: 'DELETE'
