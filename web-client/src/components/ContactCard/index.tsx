8136ba4ba1493a7da1c3b4c0c1a60617e4afc78d
Fernando Mariano Pereira <fernandovmp@outlook.com>
2020-02-03 17:21:25 -0300

    Add edit button and edit command

--- web-client/src/components/ContactCard/index.tsx
+++ web-client/src/components/ContactCard/index.tsx
@@ -2,30 +2,60 @@ import React from 'react';
 import Contact from '../../models/Contact';
 import { IconButton, IButtonStyles } from 'office-ui-fabric-react/lib/Button';
 import './styles.css';
+import { getColorFromRGBA } from 'office-ui-fabric-react';
 
 type propsType = {
     contact: Contact;
     deleteCommand: (contact: Contact) => void;
+    editCommand: (contact: Contact) => void;
 };
 
-const fabricButtonStyles: IButtonStyles = {
-    icon: [
+const baseButtonStyle: IButtonStyles = {
+    rootHovered: [
         {
-            color: '#ff0000'
+            background: getColorFromRGBA({
+                r: 255,
+                b: 255,
+                g: 255,
+                a: 20
+            }).str
         }
     ]
 };
 
-const ContactCard: React.FC<propsType> = ({ contact, deleteCommand }) => {
+const deleteButtonStyle: IButtonStyles = {
+    icon: [
+        {
+            color: '#ff0000'
+        }
+    ], ...baseButtonStyle
+};
+
+const editButtonStyle: IButtonStyles = {
+    icon: [
+        {
+            color: '#4444dd'
+        }
+    ], ...baseButtonStyle
+};
+
+const ContactCard: React.FC<propsType> = ({ contact, deleteCommand, editCommand }) => {
     return (
         <div className="contact-card">
             <header className="contact-header">
                 <strong className="name">{contact.name}</strong>
-                <IconButton
-                    iconProps={{ iconName: 'Delete' }}
-                    styles={fabricButtonStyles}
-                    onClick={e => deleteCommand(contact)}
-                />
+                <div>
+                    <IconButton
+                        iconProps={{ iconName: 'Edit' }}
+                        styles={editButtonStyle}
+                        onClick={e => editCommand(contact)}
+                    />
+                    <IconButton
+                        iconProps={{ iconName: 'Delete' }}
+                        styles={deleteButtonStyle}
+                        onClick={e => deleteCommand(contact)}
+                    />
+                </div>
             </header>
             <p>{`Email: ${contact.email ?? '?'}`}</p>
             <p>{`Telefone: ${contact.phone ?? '?'}`}</p>
